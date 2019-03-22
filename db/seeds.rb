67a6dbf851119afcddc016339d061513bf39c9b5
Barker <benbarks.v1@gmail.com>
2019-03-22 16:07:18 +0000

    seeds setup and items added&saved

---
+++ db/seeds.rb
@@ -0,0 +1,37 @@
+require_relative('../models/precious_thing.rb')
+require_relative('../models/source.rb')
+require('pry')
+
+
+precious_thing1 = PreciousThing.new({
+  "name" => "snow globe",
+  "description" => "Tiny glass ball on a base, filled with water and white bits, containing miniature London landmarks.",
+  "stock_quantity" => 2,
+  "buying_cost" => 50,
+  "selling_price" => 100
+})
+
+precious_thing2 = PreciousThing.new({
+  "name" => "hand of glory",
+  "description" => "A severed human hand, gripping an oil lamp. Shows light only to the person holding the arm.",
+  "stock_quantity" => 1,
+  "buying_cost" => 1,
+  "selling_price" => 30
+})
+
+precious_thing3 = PreciousThing.new({
+  "name" => "watch",
+  "description" => "A metal wristwatch, mens, slightly bloody. Operational.",
+  "stock_quantity" => 3,
+  "buying_cost" => 2,
+  "selling_price" => 5
+})
+
+precious_thing1.save()
+precious_thing2.save()
+precious_thing3.save()
+
+
+
+binding.pry
+nil
--- models/precious_thing.rb
+++ models/precious_thing.rb
@@ -1,22 +1,24 @@
 require_relative('./source.rb')
-
-attr_reader :id, :description, :stock_quantity, :buying_cost, :selling_price
+require_relative('../db/sql_runner.rb')
 
 class PreciousThing
 
+  attr_reader :id, :name, :description, :stock_quantity, :buying_cost, :selling_price
+
   def initialize(options)
     @id = options['id'].to_i if options['id']
+    @name = options['name']
     @description = options['description']
-    @stock_quantity = options['stock_quantity']
+    @stock_quantity = options['stock_quantity'].to_i
     @buying_cost = options['buying_cost'].to_i
     @selling_price = options['selling_price'].to_i
     # @source_id = options['source_id'].to_i
    end
 
    def save()
-    sql = 'INSERT INTO precious_things (description, stock_quantity, buying_cost, selling_price)
-            VALUES ($1, $2, $3, $4) RETURNING *'
-    values = [@description, @stock_quantity, @buying_cost, @selling_price]
+    sql = 'INSERT INTO precious_things (name, description, stock_quantity, buying_cost, selling_price)
+            VALUES ($1, $2, $3, $4, $5) RETURNING *'
+    values = [@name, @description, @stock_quantity, @buying_cost, @selling_price]
     preciosa = SqlRunner.run(sql, values)
     @id = preciosa.first()['id'].to_i
    end
