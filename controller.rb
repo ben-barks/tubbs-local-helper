1866f8821fe8e9520528ae7de83086a1dd805ce5
Barker <benbarks.v1@gmail.com>
2019-03-22 19:31:13 +0000

    controller index get added

--- controller.rb
+++ controller.rb
@@ -3,3 +3,8 @@ require('sinatra/contrib/all')
 require_relative('./models/precious_thing.rb')
 require_relative('./models/source.rb')
 also_reload('./models/*')
+
+get '/a-local-shop' do
+  @all_precious_things = PreciousThing.all()
+  erb(:index)
+end
