0be50a8888bc60c76f3cd9c07aa803fa07e108a8
Barker <benbarks.v1@gmail.com>
2019-03-23 14:21:06 +0000

    sinatra root directory altered in both controllers

--- controllers/controller.rb
+++ controllers/controller.rb
@@ -4,11 +4,13 @@ require_relative('../models/precious_thing.rb')
 require_relative('../models/source.rb')
 also_reload('../models/*')
 
+set :views, Proc.new { File.join(root, "../views/precious_things") }
+
 get '/a-local-shop' do
   @all_precious_things = PreciousThing.all()
-  erb(:"precious_things/index")
+  erb(:index)
 end
 
 get '/a-local-shop/new' do
-  erb(:"precious_things/new")
+  erb(:new)
 end
--- controllers/source_controller.rb
+++ controllers/source_controller.rb
@@ -4,7 +4,9 @@ require_relative('../models/precious_thing.rb')
 require_relative('../models/source.rb')
 also_reload('../models/*')
 
+set :views, Proc.new { File.join(root, "../views/sources") }
+
 get '/sources' do
   @all_sources = Source.all()
-  erb(:"sources/index")
+  erb (:index)
 end
