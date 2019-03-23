0ab02cf684a5a4439c94fcb80f57101ccabd36d4
Barker <benbarks.v1@gmail.com>
2019-03-23 13:08:17 +0000

    source erb created for index, folders further grouped for visibility

--- controls/source_controller.rb
+++ controls/source_controller.rb
@@ -3,3 +3,8 @@ require('sinatra/contrib/all')
 require_relative('./models/precious_thing.rb')
 require_relative('./models/source.rb')
 also_reload('./models/*')
+
+get '/a-local-shop/sources' do
+  @all_sources = Source.all()
+  erb(:sources)
+end
