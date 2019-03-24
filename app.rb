24e24642fc09d331019b3492dfd5e53db99846ec
Barker <benbarks.v1@gmail.com>
2019-03-24 12:22:50 +0000

    linked items

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ require_relative('controllers/controller.rb')
 require_relative('controllers/source_controller.rb')
 also_reload('models/*')
 
-get '/' do
+get '/a-local-shop/home' do
   erb( :index )
 end
